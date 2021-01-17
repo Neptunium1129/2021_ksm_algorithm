d52b3ece36e982f028b8b53b6f1679866d86229e
ksm <eltlwjs3@gmail.com>
2021-01-18 01:02:54 +0900

    #큰수의 법칙

---
+++ ksm/3_2.py
@@ -0,0 +1,25 @@
+#큰수의 법칙
+
+n, m, k = map(int, input().split())
+
+#n개의 수를 공백으로 구분하여 입력받기
+data = list(map(int, input().split()))
+
+data.sort()
+first = data[n-1] # 제일 큰 수
+second = data[n-2] # 2번째로 큰 수
+
+result =0
+
+while True:
+    for i in range(k):
+        if m==0:
+            break
+        result+=first
+        m -=1
+    if m==0:
+        break
+    result+=second
+    m -=1
+
+print(result)
